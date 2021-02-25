9449de5e952f8c2359c914c0d5c686831d81029a
ainurAFIFAH <ainurafifah00@users.noreply.github.com>
2021-02-25 14:31:32 +0800

    Render blog post from python file into html home page

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+	{
+		'author':'Ainur Afifah',
+		'title':'Blog Post 1',
+		'content':'First post content',
+		'date_posted':'Feb 25, 2021'
+	},
+	{
+
+		'author':'Ainur Amirah',
+		'title':'Blog Post 2',
+		'content':'First post content',
+		'date_posted':'Feb 26, 2021'
+	}
+]
+
+
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
