74e9d3151f4e733c2e929581c6a18415a3903c10
ainurAFIFAH <ainurafifah00@users.noreply.github.com>
2021-04-02 18:23:13 +0800

    Added register and login page and form

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
+
+'''
+	Token is generated from python secrets.token_hex(16)
+'''
+app.config['SECRET_KEY'] = '24de8f3a063335c5b874ee082243ac6e'
+
 
 #dummy database
 posts = [
@@ -28,6 +37,17 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register')
+def registration():
+	form = RegistrationForm()
+	return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
